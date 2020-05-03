0a2dde78e2d33bbde927d27b3f82654fad1e236a
Breno Nunes <brenontavares@gmail.com>
2020-05-03 20:28:39 -0300

    Carousel Card + Selecting body

--- src/components/elements/Card/Card.styles.js
+++ src/components/elements/Card/Card.styles.js
@@ -4,10 +4,10 @@ export const Container = styled.div`
   padding: 10px;
   background-color: #fff;
   border-radius: 10px;
-  min-width: 150px;
+  min-width: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
-  box-shadow: #00000047 0px 0px 5px 0px;
+  box-shadow: 0px 0px 40px #0C207219;
   ${props => props.style}
 `;
--- src/components/features/SelectBody/SelectBody.js
+++ src/components/features/SelectBody/SelectBody.js
@@ -2,31 +2,49 @@ import React, { useState } from "react";
 import CarouselCard from "../../modules/CarouselCard";
 import Card from "../../elements/Card";
 import Button from "../../elements/Button";
+import { useMedia } from "../../../hooks/useMedia";
 
 export default function SelectBody({ bodys, setBodySelected }) {
   const [CarouselRef, setCarouselRef] = useState(null);
   const [currentBody, setCurrentBody] = useState(bodys[0]);
+  const { isSmall } = useMedia();
 
   return (
     <>
       <CarouselCard
         reference={(Carousel) => setCarouselRef(Carousel)}
         afterChange={() => setCurrentBody(bodys[CarouselRef.getCurrentIndex()])}
+        prev={() => CarouselRef.prev()}
+        next={() => CarouselRef.next()}
         disable_box_shadow
         disable_keydown
         spread="wide"
-        style={{ minHeight: "300px" }}
+        style={{ minHeight: isSmall ? "240px" : "480px" }}
       >
         {bodys.map((body, key) => (
           <Card
             key={key}
-            style={{ padding: "40px", margin: "0 30px", minHeight: "150px" }}
+            style={{
+              padding: isSmall ? "20px" : "40px",
+              margin: isSmall ? "0 10px" : "0 30px",
+              minHeight: "150px",
+              cursor: "default",
+            }}
           >
-            <img src={body} alt={key} />
+            <img
+              src={body}
+              alt={key}
+              width={isSmall ? "70px" : "140px"}
+              height="auto"
+            />
           </Card>
         ))}
       </CarouselCard>
-      <Button action={() => setBodySelected(currentBody)} width="140px">
+      <Button
+        action={() => setBodySelected(currentBody)}
+        width="140px"
+        style={{ boxShadow: "1px 1px 4px #00000075" }}
+      >
         ESCOLHER
       </Button>
     </>
