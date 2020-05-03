5a1f5107e0807307bcebbe6e204e598bf0277307
Breno Nunes <brenontavares@gmail.com>
2020-05-03 20:24:55 -0300

    Changing images png to svg

--- src/assets/images/bodys/index.js
+++ src/assets/images/bodys/index.js
@@ -1,8 +1,8 @@
-import Ampulheta from "./Ampulheta.png";
-import Circulo from "./Circulo.png";
-import Retangulo from "./Retangulo.png";
-import TrianguloInvertido from "./Triangulo_invertido.png";
-import Triangulo from "./Triangulo.png";
+import Ampulheta from "./Ampulheta.svg";
+import Circulo from "./Circulo.svg";
+import Retangulo from "./Retangulo.svg";
+import TrianguloInvertido from "./Triangulo_invertido.svg";
+import Triangulo from "./Triangulo.svg";
 
 export default [
   Ampulheta,
