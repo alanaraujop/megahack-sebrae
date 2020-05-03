d07c7d4bf6dc2957d3241105b86360269b171991
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-05-03 17:46:24 -0300

    chore: cria modulo layout

---
+++ src/components/modules/Layout/LayoutStyled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import fundo from "../../../assets/images/fundo.png";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url(${fundo});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
