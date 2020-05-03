d90f706106652ff3ddd48260fddf41d153ca8d26
Breno Nunes <brenontavares@gmail.com>
2020-05-03 15:28:11 -0300

    Card updating (removing margins and getting props)

--- src/components/elements/Card/Card.styles.js
+++ src/components/elements/Card/Card.styles.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 30px;
-  margin: 0 20px;
+  padding: 10px;
   background-color: #fff;
   border-radius: 10px;
+  min-width: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   box-shadow: #00000047 0px 0px 5px 0px;
+  ${props => props.style}
 `;
