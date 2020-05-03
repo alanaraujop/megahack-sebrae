64b0996f1aee2bd14df8a65e2204f118db675067
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-05-03 00:25:14 -0300

    chore: implementa config de rotas/themeProvider

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { GlobalStyles } from './globalStyles';
 import { ThemeProvider } from 'styled-components';
+import theme from './theme';
 
-const addThemeProvider = (routes) => (
-  <ThemeProvider theme={ theme }>
-    <GlobalStyles/>
-    {routes}
-  </ThemeProvider>
-)
+const ContextTheme = React.createContext({
+})
+
+const addThemeProvider = (routes) => {
+  return(
+    <ContextTheme.Provider value={{}}>
+      <ThemeProvider theme={ theme }>
+        <GlobalStyles/>
+        {routes}
+      </ThemeProvider>
+    </ContextTheme.Provider>
+  )
+};
 
 export default addThemeProvider;
