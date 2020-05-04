41f414f24979e842f3726a6cb587a68acd964797
Alan Araujo <alan@estartandodevs.com.br>
2020-05-04 01:45:19 -0300

    chore: Fechar pedido

--- src/components/features/CartContent/CartContent.js
+++ src/components/features/CartContent/CartContent.js
@@ -3,11 +3,26 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CardHorizontal from "../../modules/CardHorizontal";
 import { ActionsCart } from "../../../store/actions";
-import { Content, CardContainer } from "./styles";
+import { formatMoney } from "../../../helpers";
+import { Button } from "../../elements";
+import { Content, CardContainer, Total, Row, MoreItems } from "./styles";
+import { useHistory } from "react-router-dom";
+
+const generateProductsList = (cart) =>
+  cart.reduce(
+    (acum, item, index) => `${acum}
+  *[ ${index} ]* - ${item.name} | *TAMANHO* - ${item.size}%0a`,
+    ""
+  );
 
 export default function CartContent() {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
+  const history = useHistory();
+
+  const whatsappNumber = "5521984623153";
+  const text = `*PRODUTOS:*%0a
+  -----------------%0a${generateProductsList(cart)}`;
 
   const handleRemoveItemCart = (id) => {
     dispatch(ActionsCart.removeItemCart(id));
@@ -15,6 +30,17 @@ export default function CartContent() {
 
   const handleSetSize = ({ size, id }) => {
     dispatch(ActionsCart.setSizeItemCart({ size, id }));
+  };
+
+  const getTotal = () => cart.reduce((acum, item) => item.price + acum, 0);
+
+  const closeOrder = () => {
+    dispatch(ActionsCart.clearCart());
+    window.open(
+      `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${text}`,
+      "_blank"
+    );
+    history.push("/");
   };
 
   return (
@@ -29,6 +55,30 @@ export default function CartContent() {
           />
         </CardContainer>
       ))}
+      {cart.length > 0 && (
+        <>
+          <Row>
+            <Total>TOTAL DO PEDIDO</Total>
+            <Total>{formatMoney(getTotal())}</Total>
+          </Row>
+          <MoreItems onClick={() => history.push("/")}>
+            SELECIONAR MAIS PEÇAS
+          </MoreItems>
+          <Button
+            action={closeOrder}
+            style={{
+              margin: "20px 0",
+              height: "83px",
+              maxWidth: "396px",
+              width: "100%",
+              borderRadius: "42px",
+              fontSize: "25px",
+            }}
+          >
+            FECHAR PEDIDO
+          </Button>
+        </>
+      )}
     </Content>
   );
 }
--- src/components/features/CartContent/styles.js
+++ src/components/features/CartContent/styles.js
@@ -1,24 +1,5 @@
 import styled from "styled-components";
 
-const ImageArrow = styled.img`
-  width: 18px;
-  height: 18px;
-`;
-
-const Quantity = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 46px;
-  height: 40px;
-  border: 1px solid #e8e8e8;
-  border-radius: 4px;
-  margin: 5px;
-  font-family: Roboto;
-  font-weight: 500;
-  font-size: 16px;
-`;
-
 const ContainerQuantity = styled.div`
   display: flex;
   align-items: center;
@@ -26,7 +7,9 @@ const ContainerQuantity = styled.div`
   width: 100%;
 `;
 const Content = styled.div`
-  width: 100%;
+  width: 90%;
+  max-width: 800px;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -35,11 +18,38 @@ const Content = styled.div`
 
 const Row = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
+  margin: 30px 0;
 `;
 const CardContainer = styled.div`
   width: 100%;
 `;
-export { ImageArrow, ContainerQuantity, Quantity, Content, Row, CardContainer };
+
+const TotalContainer = styled.div`
+  width: 100%;
+`;
+
+const Total = styled.p`
+  font: 25px Poppins;
+  letter-spacing: 3px;
+  color: #707070;
+`;
+const MoreItems = styled.p`
+  margin-top: 20px;
+  font: Light 20px Poppins;
+  letter-spacing: 2.4px;
+  color: #9e003d;
+  cursor: pointer;
+`;
+
+export {
+  TotalContainer,
+  Total,
+  ContainerQuantity,
+  Content,
+  Row,
+  CardContainer,
+  MoreItems,
+};
